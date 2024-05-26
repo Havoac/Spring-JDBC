7134e65cdcbb6648e071b43101501bf723ecbee4
Shivansh Singh <shivanshsingh9670@gmail.com>
2024-05-26 19:23:58 +0530

    JDBC connection done to local database | Inserting data to database

--- src/main/java/com/mavenproject2/jdbc/App.java
+++ src/main/java/com/mavenproject2/jdbc/App.java
@@ -1,5 +1,9 @@
 package com.mavenproject2.jdbc;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/mavenproject2/jdbc/config.xml");
+        
+        // we need jdbc template object to use queries
+        JdbcTemplate template = context.getBean("jdbcTemplate", JdbcTemplate.class);
+        System.out.println("My program started" );
+        // insert query
+        String query = "insert into student(id,name,city) values(?,?,?)";
+    
+        // fire query
+        int result = template.update(query, 7, "Ajay", "Patna");
+        System.out.println("no of recorded inserted = " + result);
     }
 }
